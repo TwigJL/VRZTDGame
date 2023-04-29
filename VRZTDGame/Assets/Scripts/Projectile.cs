353c0412850504ce0d57dd286e0fb500ed3ad1cd
Johnson Li <liwindx15@gmail.com>
2023-04-29 10:24:22 -0400

    Added Projectile Effects and Fine-Tuning

--- VRZTDGame/Assets/Scripts/Projectile.cs
+++ VRZTDGame/Assets/Scripts/Projectile.cs
@@ -3,7 +3,26 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed = 10f;
+    public int damage = 25;
+    public LayerMask zombieLayer;
     private Transform target;
+    public LayerMask projectileLayer;
+    public float chainRadius = 5f;
+    public float chainDelay = 0.5f;
+
+
+    public enum ProjectileEffect
+    {
+        None,
+        Slow,
+        Freeze,
+        Burn,
+        AOE,
+        Chain
+    }
+    public ProjectileEffect effect = ProjectileEffect.None;
+ 
+    public float aoeRadius = 0f;
 
     public void SetTarget(Transform target)
     {
@@ -36,8 +55,44 @@ public class Projectile : MonoBehaviour
 
     void HitTarget()
     {
-        // Implement target hit logic here
+        Collider[] zombies;
+        // Check for AOE effect and increase the radius of the OverlapSphere accordingly
+        if (effect == ProjectileEffect.AOE)
+        {
+            zombies = Physics.OverlapSphere(transform.position, aoeRadius, zombieLayer);
+        }
+        else
+        {
+            zombies = Physics.OverlapSphere(transform.position, 0.5f, zombieLayer);
+        }
+        foreach (Collider zombie in zombies)
+        {
+            ZombieBehavior zombieBehavior = zombie.GetComponent<ZombieBehavior>();
+            if (zombieBehavior != null)
+            {
+                zombieBehavior.ApplyDamage(damage);
+                // Apply the effect
+                switch (effect)
+                {
+                    case ProjectileEffect.Slow:
+                        zombieBehavior.ApplySlow();
+                        break;
+                    case ProjectileEffect.Freeze:
+                        zombieBehavior.ApplyFreeze();
+                        break;
+                    case ProjectileEffect.Burn:
+                        zombieBehavior.ApplyBurn();
+                        break;
+                     case ProjectileEffect.Chain:
+                    zombieBehavior.ApplyChainEffect(damage, chainRadius, chainDelay);
+                    break;
+                }
+            }
+        }
 
         Destroy(gameObject);
     }
+    
+
+
 }
--- VRZTDGame/Assets/Scripts/ZombieBehavior.cs
+++ VRZTDGame/Assets/Scripts/ZombieBehavior.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class ZombieBehavior : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed = 1.0f;
     public Waypoint[] waypoints;
+    private Animator animator;
     private int currentWaypoint = 0;
     private float chooseCooldown = 0.0f;
     public float chooseDelay = 1.0f;
-
+    public float slowDuration = 2f;
+    public float freezeDuration = 3f;
+    public float burnDuration = 5f;
+    public int burnDamagePerSecond = 10;
+    public int maxHealth = 100;
+    private int health;
+    private float normalSpeed;
+    private Coroutine slowCoroutine;
+    private Coroutine freezeCoroutine;
+    private Coroutine burnCoroutine;
+    public LayerMask zombieLayer;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        normalSpeed = speed;
+        health = maxHealth;
+        animator.SetFloat("Speed", speed);
+        zombieLayer = LayerMask.GetMask("Zombie");
+    }
     void Update()
     {
         if (waypoints == null || currentWaypoint >= waypoints.Length || waypoints[currentWaypoint] == null)
@@ -55,4 +74,92 @@ public class ZombieBehavior : MonoBehaviour
             currentWaypoint++;
         }
     }
+    public void ApplySlow()
+    {
+        if (slowCoroutine != null) StopCoroutine(slowCoroutine);
+        slowCoroutine = StartCoroutine(SlowEffect());
+    }
+
+    private IEnumerator SlowEffect()
+    {
+        animator.SetFloat("Speed", normalSpeed / 2f);
+        yield return new WaitForSeconds(slowDuration);
+        animator.SetFloat("Speed", normalSpeed);
+    }
+
+    public void ApplyFreeze()
+    {
+        if (freezeCoroutine != null) StopCoroutine(freezeCoroutine);
+        freezeCoroutine = StartCoroutine(FreezeEffect());
+    }
+
+    private IEnumerator FreezeEffect()
+    {
+        animator.SetFloat("Speed", 0f);
+        yield return new WaitForSeconds(freezeDuration);
+        animator.SetFloat("Speed", normalSpeed);
+    }
+
+    public void ApplyBurn()
+    {
+        if (burnCoroutine != null) StopCoroutine(burnCoroutine);
+        burnCoroutine = StartCoroutine(BurnEffect());
+    }
+
+    private IEnumerator BurnEffect()
+    {
+        float burnTime = 0f;
+
+        while (burnTime < burnDuration)
+        {
+            yield return new WaitForSeconds(1f);
+            ApplyDamage(burnDamagePerSecond);
+            burnTime += 1f;
+        }
+    }
+    public void ApplyChainEffect(int damage, float chainRadius, float chainDelay)
+    {
+        Collider[] nearbyZombies = Physics.OverlapSphere(transform.position, chainRadius, zombieLayer);
+        foreach (Collider zombie in nearbyZombies)
+        {
+            ZombieBehavior zombieBehavior = zombie.GetComponent<ZombieBehavior>();
+            if (zombieBehavior != null && zombieBehavior != this)
+            {
+                StartCoroutine(ChainEffectCoroutine(zombieBehavior, damage, chainDelay));
+            }
+        }
+    }
+
+    private IEnumerator ChainEffectCoroutine(ZombieBehavior target, int damage, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        target.ApplyDamage(damage);
+    }
+
+
+
+    public void ApplyDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            DestroyZombie();
+        }
+    }
+    private void DestroyZombie()
+    {
+        // Play death animation
+        animator.SetTrigger("Death");
+
+        // Delay the destruction of the game object
+        float delay = animator.GetCurrentAnimatorStateInfo(0).length;
+        Invoke("DestroyGameObject", delay);
+    }
+
+    private void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+
 }
