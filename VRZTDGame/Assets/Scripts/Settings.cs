a4b30bbfbea9338e03de5248c842d10d62bb751d
Johnson Li <liwindx15@gmail.com>
2023-05-07 08:27:46 -0400

    updated prefabs

--- VRZTDGame/Assets/Scripts/Settings.cs
+++ VRZTDGame/Assets/Scripts/Settings.cs
@@ -4,11 +4,13 @@ public class Settings : MonoBehaviour
 {
    public void ResumeGame()
    {
+      Time.timeScale = 1f; // Resume the game by setting the timescale to 1
       GameManager.instance.SetPauseState(false);
    }
 
    public void PauseGame()
    {
+      Time.timeScale = 0f; // Pause the game by setting the timescale to 0
       GameManager.instance.SetPauseState(true);
    }
 
