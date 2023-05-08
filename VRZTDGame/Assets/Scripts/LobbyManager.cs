c92507e6ca6c15ab9a7984efc59b271634fcb2ca
Johnson Li <liwindx15@gmail.com>
2023-05-08 00:15:54 -0400

    Finished LobbyManager Script

---
+++ VRZTDGame/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+   public GameObject mainMenu;
+   public GameObject controlsMenu;
+   public GameObject howToBuyMenu;
+   public GameObject howToPlayMenu;
+   public GameObject mapMenu;
+   public GameObject scoreboard;
+    void Start()
+    {
+      SetAllInactive();
+      mainMenu.SetActive(true);
+      scoreboard.SetActive(true);
+   }
+    public void Tutorial(){
+      SetAllInactive();
+      controlsMenu.SetActive(true);
+      howToBuyMenu.SetActive(true);
+      howToPlayMenu.SetActive(true);
+   }
+    public void BacktoMainMenu(){
+      SetAllInactive();
+      mainMenu.SetActive(true);
+      scoreboard.SetActive(true);
+   }
+    public void PlayMapsOpen(){
+    SetAllInactive();
+      mapMenu.SetActive(true);
+   }
+   public void Mountain(){
+      SceneManager.LoadScene("MountainMap");
+   }
+   public void Mall(){
+      SceneManager.LoadScene("MallMap");
+   }
+    public void Quit(){
+      Application.Quit();
+   }
+
+   // Update is called once per frame
+   private void SetAllInactive()
+   {
+      mainMenu.SetActive(false);
+      controlsMenu.SetActive(false);
+      howToBuyMenu.SetActive(false);
+      howToPlayMenu.SetActive(false);
+      mapMenu.SetActive(false);
+      scoreboard.SetActive(false);
+   }
+}
